f36aa3f399ea9132e5b54bae33bd546e94ec13a5
Xzayler <csak.mityu@gmail.com>
2022-10-18 19:25:36 +0200

    Add game function to be able to play a full round

--- script.js
+++ script.js
@@ -34,21 +34,31 @@ function getComputerChoice() {
 // This function evaluates the winner based on the oder of the
 // elements within the CHOICES list. Each element beats the
 // element before it, and loses to the element after it.
-// This game can thus be expanded
-
+// I could have made this simpler by never checking if the computer loses,
+// since if there is no computer win, and no draw, it must be
+// a computer lose, but only assuming everything else went right.
 function evaluateRound(computerSelection, playerSelection) {
+  console.log(`Computer chose: ${computerSelection}`)
+  console.log(`You chose: ${playerSelection}`)
   switch (computerSelection) {
     case playerSelection:
-      return "It's a Draw!";
+      console.log("It's a Draw!");
+      return null;
     // Does computer win?
     case CHOICES[(CHOICES.indexOf(playerSelection) + 1) % CHOICES.length]:
-      return `You lose! ${computerSelection} beats ${playerSelection}!`;
+      console.log(`You lose! ${computerSelection} beats ${playerSelection}!`);
+      return -1;
     // Does computer lose?
     case CHOICES[(CHOICES.indexOf(playerSelection) - 1 + CHOICES.length) % CHOICES.length]:
-      return `You win! ${playerSelection} beats ${computerSelection}!`;    
+      console.log(`You win! ${playerSelection} beats ${computerSelection}!`);
+      return 1;
     default:
-      return ERROR_MESSAGE;
+      console.log("Someting went wrong");
+      return null;
   }
 }
 
-console.log(evaluateRound("paper", prompt().toLowerCase()))
+function game() {
+  return evaluateRound(getComputerChoice(), prompt().toLowerCase())
+}
+game()
